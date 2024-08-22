830c69bf368f8b64e8ab65de7b2d6795013e636e
LeCitronVert <youcefphnixlord@gmail.com>
2024-08-22 21:08:29 +0200

    Showdown generation

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-import {getSeededRng} from "./props/SeededRng.mjs";
-import {PokemonBank} from "./props/PokemonBank.mjs";
-import {TrainerGenerator} from "./props/TrainerGenerator.mjs";
+import {getSeededRng} from "./scripts/SeededRng.mjs";
+import {PokemonBank} from "./scripts/PokemonBank.mjs";
+import {TrainerGenerator} from "./scripts/TrainerGenerator.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
     const pokebank = new PokemonBank(new Pokedex.Pokedex({
         cacheImages: true,
     }));
 
+    document.querySelector('.trainer-copy-showdown-team')?.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        navigator.clipboard.writeText(e.currentTarget.dataset.showdownData);
+    });
+
     document
         .querySelector('#celebrity-form')
         ?.addEventListener('submit', (event) => {
             event.preventDefault();
 
-            const name = document
-                .querySelector('#celebrity-form .name').value
-                .trim()
-            ;
-
-            const generator = getSeededRng(convertLetterToNumber(name
-                .toLowerCase()
-            ));
-            const trainer = new TrainerGenerator(generator, pokebank, name);
-
-            console.log(trainer);
+            generatePokemonTrainer(pokebank);
         })
     ;
+
+    document.addEventListener('trainerGenerated', (event) => {
+        displayTrainer(event.detail.trainer);
+    });
 })
+
+function generatePokemonTrainer (pokebank, name = null) {
+    if (null === name) {
+        name = document
+            .querySelector('#celebrity-form .name').value
+            .trim()
+        ;
+    }
+
+    const generator = getSeededRng(convertLetterToNumber(name
+        .toLowerCase()
+    ));
+    const trainer = new TrainerGenerator(generator, pokebank, name);
+}
+
+function displayTrainer (trainer) {
+    const trainerImage = document.querySelector('.trainer-image');
+    const trainerClass = document.querySelector('.trainer-class');
+    const trainerName = document.querySelector('.trainer-name');
+
+    const copyShowdownButton = document.querySelector('.trainer-copy-showdown-team');
+    copyShowdownButton.dataset.showdownData = trainer.exportInShowdownFormat();
+}
 
 /** stolen from : https://stackoverflow.com/questions/22624379/how-to-convert-letters-to-numbers-with-javascript **/
 function convertLetterToNumber(str) {
